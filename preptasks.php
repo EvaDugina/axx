f8870829f73aa5a63a0c0b2bfb236f834ceda2f6
Anton <a.zavjalov@gmail.com>
2021-12-11 11:11:49 +0300

    [Список заданий для препода] Реализован вывод информации о заданих из БД

---
+++ preptasks.php
@@ -0,0 +1,263 @@
+<?php
+	require_once("common.php");
+
+	// получение параметров запроса
+	$page_id = 0;
+	if (array_key_exists('page', $_REQUEST))
+		$page_id = $_REQUEST['page'];
+	else {
+		echo "Некорректное обращение";
+		http_response_code(400);
+		exit;
+	}
+						
+	show_header('Задания по дисциплине', 
+				array(	'Введение в разработку ПО 2021' => 'preptasks.php?page='.$page_id, 
+						'Задания по дисциплине' => 'preptasks.php?page='.$page_id
+					)
+				);
+?>
+    <main class="pt-2">
+      <div class="container-fluid overflow-hidden">
+        <div class="row gy-5">
+          <div class="col-8">
+            <div class="pt-3">
+
+              <h2>Задания по дисциплине</h2>
+<?php
+	$query = "select * from ax_task where page_id = " . $page_id . ' and status = 1 order by id';
+	$result = pg_query($dbconnect, $query);
+	
+	if (!@result || pg_num_rows($result) < 1)
+	  echo 'Задания по этой дисциплине отсутствуют';
+	else {
+?>
+              <table class="table table-hover">
+                <thead>
+                  <tr>
+                    <th scope="col"><div class="form-check"><input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/></div></th>
+                    <th scope="col" style="width:100%;">Название</th>
+                    <th scope="col"></th>
+                  </tr>
+                </thead>
+                <tbody>
+<?php
+		while ( $row = pg_fetch_assoc($result) ) {
+?>
+                  <tr>
+                    <td scope="row"><div class="form-check"><input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/></div></td>
+                    <td>
+<?php
+			if ($row['type'] == 1) {
+?>
+						<i class="fas fa-code fa-lg"></i>
+<?php
+			}
+			else {
+?>
+						<i class="fas fa-file fa-lg" style="padding: 0px 5px 0px 5px;"></i>
+<?php
+			}
+?>
+						
+						<?=$row['title']?>
+						
+<?php
+			$query = 'select students.middle_name || \' \' || students.first_name fio, ax_assignment.id aid from ax_task '.
+					 ' inner join ax_assignment on ax_task.id = ax_assignment.task_id '.
+					 ' inner join ax_assignment_student on ax_assignment.id = ax_assignment_student.assignment_id '.
+					 ' inner join students on students.id = ax_assignment_student.student_user_id '.
+					 ' where ax_task.id = '.$row['id'].
+					 ' order by ax_assignment.id';
+			$result2 = pg_query($dbconnect, $query);
+			
+			if ($result2 && pg_num_rows($result2) > 0) {
+				echo '<div class="small">Назначено:<ul><li style="display:none;">';
+				$prev_assign = 0;
+				while ($row2 = pg_fetch_assoc($result2))
+				{
+					if ($row2['aid'] == $prev_assign)
+						  echo ', '.$row2['fio'];
+					else
+						  echo '</li><li>'.$row2['fio'];
+          $prev_assign = $row2['aid'];
+				}
+				echo '</li></ul></div>';
+			}
+?>
+						
+					</td>
+                    <td class="text-nowrap">
+                      <button type="button" class="btn btn-sm px-3"><i class="fas fa-pen fa-lg"></i></button>
+                      <button type="button" class="btn btn-sm px-3"><i class="fas fa-download fa-lg"></i></button>
+                    </td>
+                  </tr>
+<?php
+		}	
+?>
+                </tbody>
+              </table>
+<?php
+	}
+?>
+
+              <h2 class="pt-5 text-secondary"><i class="fas fa-ban"></i> Архив заданий</h2>
+<?php
+	$query = "select * from ax_task where page_id = " . $page_id . ' and (status is null or status != 1) order by id';
+	$result = pg_query($dbconnect, $query);
+	
+	if (!$result || pg_num_rows($result) < 1)
+	  echo 'Архивные задания по этой дисциплине отсутствуют';
+	else {
+?>
+			  
+              <table class="table table-secondary table-hover">
+                <thead>
+                  <tr>
+                    <th scope="col"><div class="form-check"><input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/></div></th>
+                    <th scope="col" style="width:100%;">Название</th>
+                    <th scope="col"></th>
+                  </tr>
+                </thead>
+                <tbody>
+<?php
+		while ( $row = pg_fetch_assoc($result) ) {
+?>
+                  <tr>
+                    <td scope="row"><div class="form-check"><input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/></div></td>
+                    <td>
+<?php
+			if ($row['type'] == 1) {
+?>
+						<i class="fas fa-code fa-lg"></i>
+<?php
+			}
+			else {
+?>
+						<i class="fas fa-file fa-lg" style="padding: 0px 5px 0px 5px;"></i>
+<?php
+			}
+?>
+						
+						<?=$row['title']?>
+					</td>
+                    <td class="text-nowrap">
+                      <button type="button" class="btn btn-sm px-3"><i class="fas fa-undo fa-lg"></i></button>
+                      <button type="button" class="btn btn-sm px-3"><i class="fas fa-download fa-lg"></i></button>
+                    </td>
+                  </tr>
+<?php
+		}	
+?>				  
+                  <tr>
+                    <td scope="row"><div class="form-check"><input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/></div></td>
+                    <td><i class="fas fa-file fa-lg" style="padding: 0px 5px 0px 5px;"></i> #2. Атрибуты</td>
+                    <td class="text-nowrap">
+                      <button type="button" class="btn btn-sm px-3"><i class="fas fa-undo fa-lg"></i></button>
+                      <button type="button" class="btn btn-sm px-3"><i class="fas fa-download fa-lg"></i></button>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td scope="row"><div class="form-check"><input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/></div></td>
+                    <td><i class="fas fa-code fa-lg"></i> #3. Методы</td>
+                    <td class="text-nowrap">
+                      <button type="button" class="btn btn-sm px-3"><i class="fas fa-undo fa-lg"></i></button>
+                      <button type="button" class="btn btn-sm px-3"><i class="fas fa-download fa-lg"></i></button>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+<?php
+	}
+?>
+            </div>
+          </div>
+          <div class="col-4">
+            <div class="p-3 border bg-light">
+              <h6>Массовые операции</h6>
+              <div class="pt-1 pb-1"><button type="button" class="btn btn-outline-primary"><i class="fas fa-paperclip fa-lg"></i> Приложить файл</button></div>
+              <div class="pt-1 pb-1">
+                <label><i class="fas fa-users fa-lg"></i> <small>НАЗНАЧИТЬ ИСПОЛНИТЕЛЕЙ</small></label>
+              </div>
+              <div class="ps-5">
+                <section class="w-100 d-flex border">
+                  <div class="w-100 h-100 d-flex" style="margin:10px; height:150px; text-align: left;">
+                    <div id="demo-example-1" style="overflow-y: auto; height: 150px; width: 100%;">
+                      <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="" id="flexCheck1">
+                        <label class="form-check-label" for="flexCheck1">Иванов А.А.</label>
+                      </div>
+                      <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="" id="flexCheck2">
+                        <label class="form-check-label" for="flexCheck2">Петров Б.Б.</label>
+                      </div>
+                      <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="" id="flexCheck3">
+                        <label class="form-check-label" for="flexCheck3">Сидоров В.В.</label>
+                      </div>
+                      <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="" id="flexCheck4">
+                        <label class="form-check-label" for="flexCheck4">Иванова Г.Г.</label>
+                      </div>
+                      <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="" id="flexCheck5">
+                        <label class="form-check-label" for="flexCheck5">Сидоров В.В.</label>
+                      </div>
+                      <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="" id="flexCheck6">
+                        <label class="form-check-label" for="flexCheck6">Иванова Г.Г.</label>
+                      </div>
+                      <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="" id="flexCheck7">
+                        <label class="form-check-label" for="flexCheck7">Сидоров В.В.</label>
+                      </div>
+                      <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="" id="flexCheck8">
+                        <label class="form-check-label" for="flexCheck8">Иванова Г.Г.</label>
+                      </div>                                            
+                      <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="" id="flexCheck9">
+                        <label class="form-check-label" for="flexCheck9">Сидоров В.В.</label>
+                      </div>
+                      <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="" id="flexCheck10">
+                        <label class="form-check-label" for="flexCheck10">Иванова Г.Г.</label>
+                      </div>                                            
+                    </div>
+                  </div>
+                </section>
+                <section class="w-100 py-2 d-flex justify-content-center">
+                  <div class="form-outline datetimepicker w-100" style="width: 22rem">
+                    <input type="text" class="form-control active" value="2021-12-31 14:12:56" id="datetimepickerExample" style="margin-bottom: 0px;">
+                    <label for="datetimepickerExample" class="form-label" style="margin-left: 0px;">Срок выполения</label>
+                  </div>
+                </section>
+                <button type="button" class="btn btn-outline-primary"><i class="fas fa-user fa-lg"></i> Назначить индивидуально</button>
+                <button type="button" class="btn btn-outline-primary"><i class="fas fa-users fa-lg"></i> Назначить группой</button>
+              </div>
+              <div class="pt-1 pb-1">
+                <label><i class="fas fa-copy fa-lg"></i> <small>КОПИРОВАТЬ В ДИСЦИПЛИНУ</small></label>
+              </div>
+              <div class="pt-1 pb-1 align-items-center ps-5">
+                <select class="form-select" aria-label=".form-select">
+                  <option selected>Выберите дисциплину</option>
+                  <option value="1">Программирование в ЗРЛ</option>
+                  <option value="2">Методы и стандарты программирования</option>
+                  <option value="3">Компьютерная графика</option>
+                </select>
+              </div>
+              <div class="pt-1 pb-1 align-items-center ps-5">
+                  <button type="button" class="btn btn-outline-primary"><i class="fas fa-copy fa-lg"></i> Копировать</button> 
+              </div>
+              <div class="pt-1 pb-1"><button type="button" class="btn btn-outline-primary"><i class="fas fa-clone fa-lg"></i> Клонировать в этой дисциплине</button></div>
+              <div class="pt-1 pb-1"><button type="button" class="btn btn-outline-primary"><i class="fas fa-ban fa-lg"></i> Перенести в архив</button></div>
+              <div class="pt-1 pb-1"><button type="button" class="btn btn-outline-primary"><i class="fas fa-trash fa-lg"></i> Удалить</button></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+    <!-- End your project here-->
+<?php
+	show_footer();
+?>
---
+++ settings.php
@@ -0,0 +1,3 @@
+<?php
+	$DB_CONNECTION_STRING = "host=localhost port=5432 dbname=accelerator user=postgres password=123456"; 
+?>
