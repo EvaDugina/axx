4a2c4300dee86f1f64b8edcd2c19cc342926e32a
Руслан Одегов <ruslan.odegow@yandex.ru>
2022-01-10 15:27:48 +0000

    Upload New File

---
+++ utilities.php
@@ -0,0 +1,24 @@
+<?php
+
+	//семестр по цифре
+	function convert_sem_from_id($id){
+		if($id == 1) return 'Весна';
+		else return 'Осень';
+	}
+	
+	function convert_timestamp_from_string($str){
+		$pos = strpos($str, "/");
+		$year = substr($str, 0, $pos);
+		$sem = substr($str, $pos+1);
+		$sem_id = 0;
+		
+		if($sem == 'Весна') $sem_id = 1;
+		else $sem_id = 2;
+		
+		
+		
+		return array('year' => $year, 'semester' => $sem_id);
+	}
+	
+	
+?>
