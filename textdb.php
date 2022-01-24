7e2ba0496ccc7fdb7bde5d2abe32c325da0ceb7b
Артур Коваленко <arthurkov98@gmail.com>
2022-01-24 13:45:19 +0000

    Replace textdb.php

--- textdb.php
+++ textdb.php
@@ -4,13 +4,6 @@
   $file_name = 0;
   $assignment = 0;
   $responce = 0;
-  if (array_key_exists('assignment', $_REQUEST))
-    $assignment = $_REQUEST['assignment'];
-  else {
-    echo "Некорректное обращение";
-    http_response_code(400);
-    exit;
-  }
 
 
   if (array_key_exists('type', $_REQUEST))
@@ -22,25 +15,30 @@
   }
   if ($type == "open"){
 
-    if (array_key_exists('file_name', $_REQUEST))
-      $file_name = urldecode($_REQUEST['file_name']);
+    //-----------------------------------------------------------------OPEN-------------------------------------------------------
+
+    if (array_key_exists('id', $_REQUEST))
+      $id = urldecode($_REQUEST['id']);
     else {
       echo "Некорректное обращение";
       http_response_code(400);
       exit;
     }
 
-    $result2 = pg_query($dbconnect, 'select assignment_id, full_text, file_name from ax_solution_file');
+    $result2 = pg_query($dbconnect, 'select assignment_id, full_text, id from ax_solution_file');
     $result = pg_fetch_all($result2);
 
     foreach($result as $item) {
-     if($item['assignment_id'] == $assignment and $item['file_name'] == $file_name) {
+     if($item['id'] == $id) {
       $responce= $item['full_text'];
      } 
     }
 
 
   } else if ($type == "save"){
+
+    //-----------------------------------------------------------------SAVE-------------------------------------------------------
+
     if (array_key_exists('file_name', $_REQUEST))
       $file_name = urldecode($_REQUEST['file_name']);
     else {
@@ -48,17 +46,34 @@
       http_response_code(400);
       exit;
     }
-    if (array_key_exists('file', $_REQUEST))
-      $file = urldecode($_REQUEST['file']);
+    if (array_key_exists('id', $_REQUEST))
+      $id = urldecode($_REQUEST['id']);
     else {
       echo "Некорректное обращение";
       http_response_code(400);
       exit;
     }
-    pg_query($dbconnect, "UPDATE ax_solution_file SET full_text='$file' where file_name='$file_name' and assignment_id=$assignment");
+    if (array_key_exists('file', $_REQUEST)){
+      $file = urldecode($_REQUEST['file']);
+
+      pg_query($dbconnect, "UPDATE ax_solution_file SET full_text='$file', file_name='$file_name' where id='$id'");
+    }
+    else {  
+      pg_query($dbconnect, "UPDATE ax_solution_file SET file_name='$file_name' where id='$id'");
+    }
     $responce = $_REQUEST['file'];
 
   }else if ($type == "new"){
+
+    //-----------------------------------------------------------------NEW--------------------------------------------------------
+
+  if (array_key_exists('assignment', $_REQUEST))
+    $assignment = $_REQUEST['assignment'];
+  else {
+    echo "Некорректное обращение";
+    http_response_code(400);
+    exit;
+  }
     if (array_key_exists('file_name', $_REQUEST))
       $file_name = urldecode($_REQUEST['file_name']);
     else {
@@ -68,15 +83,27 @@
     } 
     pg_query($dbconnect, "INSERT INTO ax_solution_file (assignment_id, file_name, type) VALUES ('$assignment', '$file_name', '11')");
 
+    $result2 = pg_query($dbconnect, 'select assignment_id, file_name, id from ax_solution_file');
+    $result = pg_fetch_all($result2);
+
+    foreach($result as $item) {
+     if($item['assignment_id'] == $assignment and $item['file_name'] == $file_name) {
+      $responce= $item['id'];
+     } 
+    }
+
   }else if ($type == "del"){
-    if (array_key_exists('file_name', $_REQUEST))
-      $file_name = urldecode($_REQUEST['file_name']);
+    
+    //-----------------------------------------------------------------DEL---------------------------------------------------------
+
+    if (array_key_exists('id', $_REQUEST))
+      $id = urldecode($_REQUEST['id']);
     else {
       echo "Некорректное обращение";
       http_response_code(400);
       exit;
-    } 
-    pg_query($dbconnect, "DELETE FROM ax_solution_file WHERE file_name='$file_name' and assignment_id=$assignment");
+    }
+    pg_query($dbconnect, "DELETE FROM ax_solution_file WHERE id='$id'");
 
   }
 ?>
