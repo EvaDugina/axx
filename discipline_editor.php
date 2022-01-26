14325ff1231531dd106cd2cbb2bf78ed677645f1
Артур Коваленко <arthurkov98@gmail.com>
2022-01-26 20:34:15 +0000

    Replace discipline_editor.php

--- discipline_editor.php
+++ discipline_editor.php
@@ -24,7 +24,6 @@
 		$result = pg_query($dbconnect, $query);
 		$page = pg_fetch_all($result)[0];
 		
-		
 		foreach($disciplines as $key => $discipline){
 			if($discipline['id'] == $page['disc_id'])
 				$name = $discipline['name'];
@@ -34,8 +33,25 @@
 		$result = pg_query($dbconnect, $query);
 		$timestamps = pg_fetch_all($result);
 		
+		$query = select_teacher_name();
+		$result = pg_query($dbconnect, $query);
+		$teachers = pg_fetch_all($result);
+		
+		$query = select_page_prep_name($page_id);
+		$result = pg_query($dbconnect, $query);
+		$actual_teachers = pg_fetch_all($result);
+		
+		$query = select_groups();
+		$result = pg_query($dbconnect, $query);
+		$groups = pg_fetch_all($result);
+		
+		$query = select_discipline_groups($page_id);
+		$result = pg_query($dbconnect, $query);
+		$page_groups = pg_fetch_all($result);
+		
+		
 		#echo "<pre>";
-		#var_dump($timestamps);
+		#var_dump(json_decode($page_groups_json));
 		#echo "</pre>";
 	?>
 
@@ -138,21 +154,18 @@
 			<div class="col-2 row justify-content-left">Полное название</div>
 			<div class="col-4">
 				<div class="btn-group shadow-0">
-				  <button
-					type="button"
-					class="btn btn-light dropdown-toggle"
-					data-mdb-toggle="dropdown"
-					aria-expanded="false"
-				  >
-					<?=$name?>
-				  </button>
-				  <ul class="dropdown-menu">
+				  <select class="form-select" name = "timestamp">
+					<option selected>
+						<?=$name?>
+					</option>
 					<?php
 						foreach($disciplines as $discipline){
-							echo "<li><a class='dropdown-item' href='#'>".$discipline['name']."</a></li>";
+							if($discipline['name'] == $name)
+								continue;
+							echo "<option>".$discipline['name']."</option>";
 						}
 					?>
-				  </ul>
+				  </select>
 				</div>
 			</div>
 		</div>
@@ -177,7 +190,7 @@
 			</div>
 		</div>
 		
-		<div class="row align-items-center m-3" style="height: 40px;">
+		<div class="row align-items-center m-3">
 			<div class="col-2 row justify-content-left">Краткое название</div>
 			<div class="col-4">
 				<div class="form-outline" style="width:250px;">
@@ -186,21 +199,53 @@
 			</div>
 		</div>
 		
-		<div class="row align-items-center m-3" style="height: 40px;">
+		<div class="row align-items-center m-3" id="teachers_container">
 			<div class="col-2 row justify-content-left">Преподаватели</div>
-			<div class="col-2">Иван Сергеевич<button type="button" class="btn-close" aria-label="Close"></button></div>
+		</div>
+		
+		<div class="row align-items-center m-3">
+			<div class="col-2 row justify-content-left"></div>
+			
+			<div class="col-2">
+				<div class="btn-group shadow-0">
+					  <select class="form-select" id = "select_teacher">
+						<?php
+							foreach($teachers as $teacher){
+								echo "<option>".$teacher['first_name'].' '.$teacher['middle_name']."</option>";
+							}
+						?>
+					  </select>
+				</div>
+			</div>
+		
 			<div class="col-1">
-				<button type="button" class="btn btn-outline-primary" data-mdb-ripple-color="dark" style="width:120px;">
+				<button type="button" class="btn btn-outline-primary" data-mdb-ripple-color="dark" style="width:120px;" id = "add_teachers">
 				  Добавить 
 				</button>
 			</div>
 		</div>
 		
-		<div class="row align-items-center m-3" style="height: 40px;">
+		<div class="row align-items-center m-3" id = "groups_container">
 			<div class="col-2 row justify-content-left">Учебные группы</div>
-			<div class="col-2">КММО-02-20<button type="button" class="btn-close" aria-label="Close"></button></div>
+		</div>
+		
+		<div class="row align-items-center m-3">
+			<div class="col-2 row justify-content-left"></div>
+			
+			<div class="col-2">
+				<div class="btn-group shadow-0">
+					  <select class="form-select" name = "page_group" id = "select_groups">
+						<?php
+							foreach($groups as $page_group){
+								echo "<option>".$page_group['name']."</option>";
+							}
+						?>
+					  </select>
+				</div>
+			</div>
+		
 			<div class="col-1">
-				<button type="button" class="btn btn-outline-primary" data-mdb-ripple-color="dark" style="width:120px;">
+				<button type="button" class="btn btn-outline-primary" data-mdb-ripple-color="dark" style="width:120px;" id = "add_groups">
 				  Добавить 
 				</button>
 			</div>
@@ -241,6 +286,90 @@
     <!-- MDB -->
     <script type="text/javascript" src="js/mdb.min.js"></script>
     <!-- Custom scripts -->
-    <script type="text/javascript"></script>
+    <script type="text/javascript">
+
+		let teachers = new Set();
+		let groups = new Set();
+		
+		var actual_teachers_json = <?php echo json_encode($actual_teachers); ?>;
+		var page_groups_json = <?php echo json_encode($page_groups); ?>;
+		
+		actual_teachers_json.forEach(function(r){
+			let name = r.first_name + ' ' + r.middle_name;
+			add_element(document.getElementById("teachers_container"), name, "teachers[]", teachers);
+			teachers.add(name);
+		});
+		
+		page_groups_json.forEach(function(r){
+			let name = r.name;
+			add_element(document.getElementById("groups_container"), name, "groups[]", groups);
+			groups.add(name);
+		});
+
+		let add_teachers_button = document.getElementById("add_teachers");
+		add_teachers_button.addEventListener('click', add_teacher);
+		
+		let add_groups_button = document.getElementById("add_groups");
+		add_groups_button.addEventListener('click', add_groups);
+		
+		
+		function add_teacher()
+		{
+			let name = document.getElementById("select_teacher").value;
+			
+			if(teachers.has(name))
+				return;
+			
+			add_element(document.getElementById("teachers_container"), name, "teachers[]", teachers);
+
+			teachers.add(name);
+		}
+		
+		function add_groups()
+		{
+			let name = document.getElementById("select_groups").value;
+			
+			if(groups.has(name))
+				return;
+			
+			add_element(document.getElementById("groups_container"), name, "groups[]", groups);
+
+			groups.add(name);
+		}
+		
+		function add_element(parent, name, tag, set)
+		{
+			let element = document.createElement("div");
+
+			element.classList.add("col-2");
+			element.innerText = name;
+			
+			let button = document.createElement("button");
+			button.classList.add("btn-close");
+
+			button.setAttribute("aria-label","Close");
+			button.setAttribute("type","button");
+			
+			element.append(button);
+			parent.append(element);
+			
+			button.addEventListener('click', function (event){
+
+				let name = event.target.parentNode.textContent;
+				set.delete(name);
+				parent.removeChild(event.target.parentNode);
+			});
+			
+			let input = document.createElement("input");
+			input.setAttribute("type","hidden");
+			input.setAttribute("value", name);
+			input.setAttribute("name", tag);
+			element.append(input);
+		}
+		
+		
+
+	
+	</script>
   </body>
 </html>
