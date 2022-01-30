eaff7b0e01e978a5fd2d4187cd6e0dee715e3bb4
Артур Коваленко <arthurkov98@gmail.com>
2022-01-30 17:44:20 +0000

    Upload New File

---
+++ task_utilities.php
@@ -0,0 +1,34 @@
+<?php
+	
+	require_once("common.php");
+	require_once("dbqueries.php");
+	
+	if($_POST['type'] == 1){
+		$query = select_task_file(2, $_POST['task_id']);
+		$result = pg_query($dbconnect, $query);
+		$file = pg_fetch_all($result);
+		if(empty($file))
+			$query = insert_file(2, $_POST['task_id'], "test.cpp", $_POST['full_text_test']);
+		else
+			$query = update_file(2, $_POST['task_id'], $_POST['full_text_test']);
+		
+		$result = pg_query($dbconnect, $query);
+		
+
+		$query = select_task_file(3, $_POST['task_id']);
+		$result = pg_query($dbconnect, $query);
+		$file = pg_fetch_all($result);		
+		if(empty($file))
+			$query = insert_file(3, $_POST['task_id'], "checktest.cpp", $_POST['full_text_test_of_test']);
+		else
+			$query = update_file(3, $_POST['task_id'], $_POST['full_text_test_of_test']);
+		
+		$result = pg_query($dbconnect, $query);
+	}
+	
+	$query = update_task($_POST['task_id'], $_POST['type'], $_POST['title'], $_POST['description']);
+	$result = pg_query($dbconnect, $query);
+	$file = pg_fetch_all($result);
+	
+	header('Location: http://localhost/accelerator/preptasks.php?page=-1#');
+?>
