211019b4c9cebedcf1cd33b4a03b9ecf51910c7b
Георгий Гагуа <gagua.georgy@gmail.com>
2022-01-31 20:52:01 +0000

    Внес правки

--- mainpageSt.php
+++ mainpageSt.php
@@ -5,7 +5,8 @@ require_once("dbqueries.php");
 
   show_header('Дэшборд студента', array('Дэшборд студента' => 'mainpageSt.php'));
 
-$result2 = pg_query($dbconnect, 'select year, short_name,semester, id, status from ax_page');
+
+$result2 = pg_query($dbconnect, 'select year, short_name,semester,name, status,disc_id, ax_page.id from (ax_page inner join discipline ON ax_page.disc_id = discipline.id) ORDER BY year DESC');
 $result = pg_fetch_all($result2);
 
 
@@ -26,11 +27,11 @@ foreach($result as $item) {
 <main class="pt-2 justify-content-between">
       <h2 class= "row" style="margin-top: 30px; margin-left: 50px">1 семестр</h2>
  <div class="container">
- <div class="row d-flex justify-content-between">
+ <div class="row">
  <?php foreach($first_semestr as $item) { ?>
           
-          <div class="col-3 border border-dark">
-            <a href="<?='accel/task4students.html?page='. $item['id']?>"><?=$item['short_name']?></a>
+          <div class="col-3">
+            <a href="<?='/task4students.html?page='. $item['id']?>"><?=$item['short_name']?></a>
                 <div class="d-flex justify-content-between" style="margin-top: 60px;">
                     <span>Выполнено</span>
                     <span>10/12</span>
@@ -38,7 +39,7 @@ foreach($result as $item) {
               <div class="progress" style="height: 20px">
                 <div class="progress-bar" role="progressbar" style="width: 80%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">80%</div>
               </div>
-              <div><small id="" class="form-text text-muted">Проектирование пользовательских<br> интерфейсов</small></div>
+              <div><small id="" class="form-text text-muted"><?=$item['name']?></small></div>
           </div>
   <?php } ?>
 </div>
@@ -46,9 +47,9 @@ foreach($result as $item) {
  
  <h2 class= "row" style="margin-top: 30px; margin-left: 50px">2 семестр</h2>
 <div class="container">
-<div class="row d-flex justify-content-between">
+<div class="row">
  <?php foreach($second_semestr as $item) { ?>
-          <div class="col-3 border border-dark">
+          <div class="col-3" style="margin-top: 10px;">
             <a href=""><?=$item['short_name']?></a>
                 <div class="d-flex justify-content-between" style="margin-top: 60px;">
                     <span>Выполнено</span>
@@ -57,7 +58,7 @@ foreach($result as $item) {
               <div class="progress" style="height: 20px">
                 <div class="progress-bar" role="progressbar" style="width: 80%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">80%</div>
               </div>
-              <div><small id="" class="form-text text-muted">Проектирование пользовательских<br> интерфейсов</small>
+              <div><small id="" class="form-text text-muted"><?=$item['name']?></small>
               </div>
           </div>
   <?php } ?>
