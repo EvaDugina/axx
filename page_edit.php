b0540702bc515cde486a55b2dcc54434d31e4779
Артур Коваленко <arthurkov98@gmail.com>
2022-02-01 16:12:37 +0000

    Replace page_edit.php

--- page_edit.php
+++ page_edit.php
@@ -3,7 +3,9 @@
 	require_once("common.php");
 	require_once("dbqueries.php");
 	require_once("utilities.php");
-
+	
+	$id = $_POST['id'];
+	
 	if ($_POST['id'] != 0) {
 	
 		$query = update_discipline($_POST);
@@ -15,25 +17,34 @@
 		$query = delete_page_group($_POST['id']);
 		$result = pg_query($dbconnect, $query);
 	} else {
+		
 		$query = insert_discipline($_POST);
 		$result = pg_query($dbconnect, $query);
+		$id = pg_fetch_all($result)[0]['id'];
 	}
-
+	/*
+	echo "<pre>";
+	var_dump($_POST);
+	echo "</pre>";
+	echo "<pre>";
+	var_dump($id);
+	echo "</pre>";
+	*/
 	foreach($_POST['teachers'] as $teacher)
 	{
 		$pos= strpos($teacher, ' ');
 		$first_name = substr($teacher, 0, $pos);
 		$middle_name = substr($teacher, $pos+1);
-		$query = prep_ax_prep_page($_POST['id'], $first_name, $middle_name);
+		$query = prep_ax_prep_page($id, $first_name, $middle_name);
 		pg_query($dbconnect, $query);
 	}
 	
 	foreach($_POST['groups'] as $group)
 	{
-		$query = update_ax_page_group($_POST['id'], $group);
+		$query = update_ax_page_group($id, $group);
 		pg_query($dbconnect, $query);
 	}
 
-	#var_dump($_POST);
+	var_dump($id);
 	header('Location: mainpage.php');
 ?>
