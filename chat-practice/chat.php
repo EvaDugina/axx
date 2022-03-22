8b64623cf408de63b762efa7c3d4f7b11d1d1c69
Daniil Kurov <kurov.daniil@mail.ru>
2022-03-22 12:19:35 +0300

    пробная№2 Не получается считать txt

---
+++ chat-practice/chat.php
@@ -0,0 +1,20 @@
+
+<head>
+<link rel="stylesheet" href="style.css" />
+<script type="text/javasctipt" src="func.js"> 
+</script>
+
+</head>
+
+<div class='chat'>
+	<div class='chat-messages'>
+		<div class='chat-messages__content' id='messages'>
+			Загрузка...
+		</div>
+	</div>
+	<div class='chat-input'>
+		<form method='post' id='chat-form'>
+			<input type='text' id='message-text' class='chat-form__input' placeholder='Введите сообщение'> <input type='submit' class='chat-form__submit' value='=>'>
+		</form>
+	</div>
+</div>
