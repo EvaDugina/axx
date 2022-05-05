6ed92acf7665ae13c31d0448f1612f603d3f9428
Eduard Guliev <guliev.eduardrd@mail.ru>
2022-05-05 10:28:46 +0300

    Cleaned up code

--- post.php
+++ post.php
@@ -2,12 +2,9 @@
 session_start();
 if(isset($_SESSION['login'])){
 	$text = $_POST['text'];
-	
-	
-$fp = fopen("log.html", 'a');
+	$fp = fopen("log.html", 'a');
 	fwrite($fp, "<div class='msgln'> 
-	<div
- 	class='msgln-message'><b>".$_SESSION['username']."</b>
+	<div class='msgln-message'><b>".$_SESSION['username']."</b>
 	<br> ".stripslashes(htmlspecialchars($text))."</div>
 	<div class='file-input'>
 	</div>
