ef4c8979559ed69494a0454e177dcc087134774d
Eduard Guliev <guliev.eduardrd@mail.ru>
2022-05-07 19:59:22 +0300

    Cleaned code, changed class names

--- post.php
+++ post.php
@@ -3,15 +3,18 @@ session_start();
 if(isset($_SESSION['login'])){
 	$text = $_POST['text'];
 	$fp = fopen("log.html", 'a');
-	fwrite($fp, "<div class='msgln'> 
-	<div class='msgln-message'><b>".$_SESSION['username']."</b>
-	<br> ".stripslashes(htmlspecialchars($text))."</div>
-	<div class='file-input'>
-	</div>
-	<div
-	 	class='msgln-date'>".date('d.m.Y H:i')."
-	</div>
-	</div>");
+	fwrite($fp, '
+		<div class="chat-box-message">
+			<!-- В этом диве имя пользователя, сообщение и файлы прикрепленные -->
+			<div class="chat-box-message-wrapper"> 
+				<b>' . $_SESSION['username'] . '</b><br>'
+				. stripslashes(htmlspecialchars($text)) . '
+			</div>
+			<div class="chat-box-message-date">
+				' . date('d.m.Y H:i', time()) . '
+			</div>
+		</div>
+	');
 fclose($fp);
 }
 ?> 
