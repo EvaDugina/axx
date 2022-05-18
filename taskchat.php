fca9644ebaf560acfe1ba28701c3baa0dc5ead8d
Eduard Guliev <guliev.eduardrd@mail.ru>
2022-05-18 19:27:49 +0300

    Started to use show_header_2. Added task finish date time

--- taskchat.php
+++ taskchat.php
@@ -25,9 +25,9 @@ if ($row) {
 	$assignment_id = $row['id'];
 }
 
-$query = select_all_disciplines();
-$result = pg_query($dbconnect, $query);
-$disciplines = pg_fetch_all($result);
+// $query = select_all_disciplines();
+// $result = pg_query($dbconnect, $query);
+// $disciplines = pg_fetch_all($result);
 
 $task_title = '';
 $task_description = '';
@@ -63,13 +63,13 @@ if ($task_status_code != '') {
 	$task_status_text = $task_status_texts[$task_status_code];
 }
 
-$discipline_name = '';
+$discipline_short_name = '';
 if ($page_id) {
-	$query = "select d.name as \"discipline\" from ax_page p inner join discipline d on p.disc_id = d.id where p.id = $page_id;";
+	$query = "select short_name from ax_page where id = $page_id";
 	$result = pg_query($dbconnect, $query);
 	$row = pg_fetch_assoc($result);
 	if ($row) {
-		$discipline_name = $row['discipline'];
+		$discipline_short_name = $row['short_name'];
 	}
 }
 
@@ -88,11 +88,12 @@ function show_task_requirements_links() {
 //вытягивание сообщений из бд
 //для каждого задания для каждого пользователя assignment_id уникальный 
 //(для Задание 3. Классификация языка и грамматики (task_id = -14) и пользователя ivan assignment_id = -22)
-function select_chat_messages($user_id) {
-	global $dbconnect, $task_id, $assignment_id;
-	$query = "select ax_message.date_time, ax_message.full_text, students.first_name, students.middle_name 
-		from ax_message
-		inner join ax_assignment on ax_message.assignment_id = ax_assignment.id
+$task_finish_date_time = '';
+//Если в ax_assignment status_code = 3, значит задание проверено и в ax_message есть сообщение от препода со статусом 2, в котором он ставит оценку
+function select_chat_messages() {
+	global $dbconnect, $assignment_id, $task_finish_date_time;
+	$query = "SELECT ax_message.type, ax_message.date_time, ax_message.full_text, students.first_name, students.middle_name
+		from ax_message inner join ax_assignment on ax_message.assignment_id = ax_assignment.id
 		inner join students on ax_message.sender_user_id = students.id
 		where ax_message.assignment_id = $assignment_id order by date_time";
 	$result = pg_query($dbconnect, $query) or die('Ошибка запроса: ' . pg_last_error());
@@ -105,117 +106,130 @@ function select_chat_messages($user_id) {
 		$date_time = $date[2] .".". $date[1] .".". $date[0] ." ". $time[0] .":". $time[1];
 		$username = $row['first_name'] . ' ' . $row['middle_name'];
 		$ret[] = array('date_time' => $date_time, 'full_text' => $row['full_text'], 'username' => $username);
+
+		if ($row['type'] == 2) { $task_finish_date_time = $date_time; }
 		$row = pg_fetch_assoc($result);
 	}
+	// foreach ($ret as $row) {
+	// 	foreach ($row as $k => $v) {
+	// 		echo "$k => $v<br>";
+	// 	}
+	// }
 	return $ret;
 }
-
-
-show_header('Чат', array($discipline_name => 'studtasks.php?page='. $page_id));
+$user_messages = select_chat_messages();
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<?php show_head('Чат с преподавателем'); ?>
 <link rel="stylesheet" href="taskchat.css">
 <!-- Font Awesome -->
 <script src="https://kit.fontawesome.com/1dec1ea41f.js" crossorigin="anonymous"></script>
-<main>
-    <div class="task-wrapper">
-        <h2><?= $task_title ?></h2>
-        <div>
-			<div class="task-desc-wrapper">
-				<p><?= $task_description ?></p>
-				<p><b>Требования к выполнению и результату:</b><br> <?php show_task_requirements_links(); ?></p>
-				<div>
-					<p><b>Срок выполнения: </b> <?= $task_finish_limit ?></p>
-					<a href="https://vega.fcyb.mirea.ru/" class="task-download-button" target="_blank"><i class="fa-solid fa-file-arrow-down"></i><span>Скачать задание</span></a>
-				</div>
-			</div>
-			<div class="task-status-wrapper">
-				<div>
-					<div style="display:flex; align-items:center">
-						<div class="check-mark" style="<?= $task_status_code == 3 ? 'background:#0f0' : '' ?>">
-							<i class="fa-solid fa-check"></i>
-						</div>
-						<b><?= $task_status_text ?></b>
-					</div>
-					<?php if ($task_status_code == 3 || $task_status_code == 5): ?>
-						<br><br>
-						21.10.2021 17:34 <br>
-						Оценка: <b><?= $task_mark ?></b>
-					<?php endif; ?>
-				</div>
-				<a href="https://vega.fcyb.mirea.ru/" class="code-redactor-button" target="_blank"><i class="fa-solid fa-file-pen"></i>Онлайн редактор кода</a>
-			</div>
-		</div>
-    </div>
-    <div class="chat-wrapper">
-		<div id="chat-box">
-			<?php
-			// if (file_exists("log.html") && filesize("log.html") > 0) {
-			// 	$handle = fopen("log.html", "r");
-			// 	$contents = fread($handle, filesize("log.html"));
-			// 	fclose($handle);
-			// 	echo $contents;}
-			//работа с бд ---->
-			$user_messages = select_chat_messages($user_id);
-			foreach ($user_messages as $v) {
-				echo '
-				<div class="chat-box-message">
-					<div class="chat-box-message-wrapper">
-						<b>' . $v['username'] . '</b><br>'
-						. stripslashes(htmlspecialchars($v['full_text'])) . '
-					</div>
-					<div class="chat-box-message-date">
-						' . $v['date_time'] . '
-					</div>
-				</div>
-				';
-			}
-			?>
-		</div>
-		<form action="" method="POST" class="message-form">
-			<span>Сообщение:</span>
-			<input type="text" name="user-message" id="user-message" placeholder="Напишите сообщение...">
-			<button type="submit" name="submit-message" id="submit-message">Отправить</button>
-		</form>
-		<form action="post.php" method="POST" enctype="multipart/form-data">
-			<span>Вложения:</span>
-			<input id="file-input" class="input-file" type="file" name="filename">
-		</form>	
-    </div>
-	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js"></script>
-	<script type="text/javascript">
-	// jQuery Document
-	$(document).ready(function(){
-		
-		//If user submits the form
-		$("#submit-message").click(function(){	
-			var userMessage = $("#user-message").val();
-			$.post("post.php", {text: userMessage});				
-			$("#user-message").attr("value", "");
-			return false;
-		});
-		
-		//Load the file containing the chat log
-		// function loadLog() {		
-		// 	var oldscrollHeight = $("#chat-box").attr("scrollHeight") - 20;
-		// 	$.ajax ({
-		// 		url: "log.html",
-		// 		cache: false,
-		// 		success: function(html) {		
-		// 			$("#chat-box").html(html); //Insert chat log into the #chat-box
-		// 		// for ($m = 0; $m < count($messages); $m++) { // list all messages
-		// 		//   if ($messages[$m]['mtype'] != null)
-		// 		//     show_message($messages[$m]);
-		// 		// }
-		// 			var newscrollHeight = $("#chat-box").attr("scrollHeight") - 20;
-		// 			if(newscrollHeight > oldscrollHeight) {
-		// 				$("#chat-box").animate({ scrollTop: newscrollHeight }, 'normal'); //Autoscroll to bottom of div
-		// 			}				
-		// 		},
-		// 	});
-		// }
-		setInterval (select_chat_messages($user_id), 2000);	//Reload ax_messages every 2 seconds
-	});
-	</script>
-</main>
 
-<?php show_footer(); ?>
+<body>
+	<?php show_header_2($dbconnect, 'Чат c перподавателем', 
+		array('Дэшборд студента' => 'mainpage_student.php', $discipline_short_name => 'studtasks.php?page='. $page_id, $task_title => '')); ?>
+	<main>
+		<div class="task-wrapper">
+			<h2><?= $task_title ?></h2>
+			<div>
+				<div class="task-desc-wrapper">
+					<p><?= $task_description ?></p>
+					<p><b>Требования к выполнению и результату:</b><br> <?php show_task_requirements_links(); ?></p>
+					<div>
+						<p><b>Срок выполнения: </b> <?= $task_finish_limit ?></p>
+						<a href="https://vega.fcyb.mirea.ru/" class="task-download-button" target="_blank"><i class="fa-solid fa-file-arrow-down"></i><span>Скачать задание</span></a>
+					</div>
+				</div>
+				<div class="task-status-wrapper">
+					<div>
+						<div style="display:flex; align-items:center">
+							<div class="check-mark" style="<?= $task_status_code == 3 ? 'background:#0f0' : '' ?>">
+								<i class="fa-solid fa-check"></i>
+							</div>
+							<b><?= $task_status_text ?></b>
+						</div>
+						<?php if ($task_status_code == 3 || $task_status_code == 5): ?>
+							<br><br>
+							<?= $task_finish_date_time ?> <br>
+							Оценка: <b><?= $task_mark ?></b>
+						<?php endif; ?>
+					</div>
+					<a href="https://vega.fcyb.mirea.ru/" class="code-redactor-button" target="_blank"><i class="fa-solid fa-file-pen"></i>Онлайн редактор кода</a>
+				</div>
+			</div>
+		</div>
+		<div class="chat-wrapper">
+			<div id="chat-box">
+				<?php
+				// if (file_exists("log.html") && filesize("log.html") > 0) {
+				// 	$handle = fopen("log.html", "r");
+				// 	$contents = fread($handle, filesize("log.html"));
+				// 	fclose($handle);
+				// 	echo $contents;}
+				//работа с бд ---->
+				foreach ($user_messages as $v) {
+					echo '
+					<div class="chat-box-message">
+						<div class="chat-box-message-wrapper">
+							<b>' . $v['username'] . '</b><br>'
+							. stripslashes(htmlspecialchars($v['full_text'])) . '
+						</div>
+						<div class="chat-box-message-date">
+							' . $v['date_time'] . '
+						</div>
+					</div>
+					';
+				}
+				?>
+			</div>
+			<form action="" method="POST" class="message-form">
+				<span>Сообщение:</span>
+				<input type="text" name="user-message" id="user-message" placeholder="Напишите сообщение...">
+				<button type="submit" name="submit-message" id="submit-message">Отправить</button>
+			</form>
+			<form action="post.php" method="POST" enctype="multipart/form-data">
+				<span>Вложения:</span>
+				<input id="file-input" class="input-file" type="file" name="filename">
+			</form>	
+		</div>
+		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js"></script>
+		<script type="text/javascript">
+		// jQuery Document
+		$(document).ready(function(){
+			
+			//If user submits the form
+			$("#submit-message").click(function(){	
+				var userMessage = $("#user-message").val();
+				$.post("post.php", {text: userMessage});				
+				$("#user-message").attr("value", "");
+				return false;
+			});
+			
+			//Load the file containing the chat log
+			// function loadLog() {		
+			// 	var oldscrollHeight = $("#chat-box").attr("scrollHeight") - 20;
+			// 	$.ajax ({
+			// 		url: "log.html",
+			// 		cache: false,
+			// 		success: function(html) {		
+			// 			$("#chat-box").html(html); //Insert chat log into the #chat-box
+			// 		// for ($m = 0; $m < count($messages); $m++) { // list all messages
+			// 		//   if ($messages[$m]['mtype'] != null)
+			// 		//     show_message($messages[$m]);
+			// 		// }
+			// 			var newscrollHeight = $("#chat-box").attr("scrollHeight") - 20;
+			// 			if(newscrollHeight > oldscrollHeight) {
+			// 				$("#chat-box").animate({ scrollTop: newscrollHeight }, 'normal'); //Autoscroll to bottom of div
+			// 			}				
+			// 		},
+			// 	});
+			// }
+			setInterval (select_chat_messages($user_id), 2000);	//Reload ax_messages every 2 seconds
+		});
+		</script>
+	</main>
+</body>
+</html>
