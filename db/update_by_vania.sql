a735b909b19273cfe55765e0da05cb642adec97d
JohnDugin <vania.dugin917@mail.ru>
2022-09-07 01:40:43 +0300

    pageedit.phph, issue #37

--- db/update_by_vania.sql
+++ db/update_by_vania.sql
@@ -7,15 +7,16 @@ UPDATE ax_assignment SET status_code = 5, status_text = 'ожидает пров
 DROP TABLE ax_color_theme;
 CREATE TABLE ax_color_theme (
     id SERIAL, 
+    disc_id INTEGER,
     name text, 
     bg_color text, 
     src_url text, 
     CONSTRAINT ax_color_theme_pkey PRIMARY KEY (id)
 ); ALTER TABLE ax_color_theme OWNER TO accelerator;
 
-INSERT INTO public.ax_color_theme(id, name, bg_color, src_url) VALUES
-(0, 'Красный', '#dc3545', 'src/img/red.jpg'), (1, 'Жёлтый', '#ffc107', 'src/img/yellow.jpg'), 
-(2, 'Зелёный', '#198754', 'src/img/green.jpg'), (3, 'Синий', '#1266f1', 'src/img/blue.jpg'),
-(4, 'Фиолетовый', '#6f42c1', 'src/img/purple.jpg');
+INSERT INTO public.ax_color_theme(id, disc_id, name, bg_color, src_url) VALUES
+(0, -1, 'Красный', '#dc3545', 'src/img/red.jpg'), (1, -2, 'Жёлтый', '#ffc107', 'src/img/yellow.jpg'), 
+(2, -3, 'Зелёный', '#198754', 'src/img/green.jpg'), (3, -4, 'Синий', '#1266f1', 'src/img/blue.jpg'),
+(4, -5, 'Фиолетовый', '#6f42c1', 'src/img/purple.jpg');
 
 UPDATE ax_page SET color_theme_id = 0;
