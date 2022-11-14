ed93397ae67d0bc407d15a814b256500af72bf71
JohnDugin <vania.dugin917@mail.ru>
2022-11-14 16:28:18 +0300

    fix: Исправлена ф-ция get_semester из БД

--- db/update_20221103.sql
+++ db/update_20221103.sql
@@ -2,9 +2,9 @@ CREATE OR REPLACE FUNCTION get_semester(year integer, semester integer)
   RETURNS text AS
 $BODY$
 BEGIN
-    IF (semester = 1) THEN
+    IF (semester%2 = 1) THEN
 	RETURN (year-1)::text || '/' || year || ' весна';
-    ELSIF (semester = 2) THEN
+    ELSIF (semester%2 = 0) THEN
         RETURN year || '/' || (year+1)::text || ' осень';
     END IF;
 
