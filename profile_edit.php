98e38c059665fdb83b456799e975718307dfd5c3
avolkov00 <vania.dugin917@mail.ru>
2023-04-28 18:09:25 +0300

    fix: slash to UNIX

--- profile_edit.php
+++ profile_edit.php
@@ -2,7 +2,7 @@
 require_once("common.php");
 require_once("dbqueries.php");
 require_once("utilities.php");
-require_once("POClasses\User.class.php");
+require_once("POClasses/User.class.php");
 
 $user = new User((int)$_SESSION['hash']);
 
