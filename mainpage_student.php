a29a818d5e335e2429832d847c2b5df5baafb1c3
avolkov00 <vania.dugin917@mail.ru>
2023-05-15 14:48:34 +0300

    fix: mainpage_student - Отрицательные семестры

--- mainpage_student.php
+++ mainpage_student.php
@@ -47,101 +47,85 @@ $Group = new Group((int)$group_id);
             $array_semester = 0;
             foreach($pages as $page_id){
               $Page = new Page((int)$page_id['id']);
-              if($Page->year != $array_year || $Page->semester != $array_semester) {
+              $semester_number = getSemesterNumberByGroup($Page->year, $Page->semester, $Group->year);
+              if ($semester_number > 0) {
+                if($Page->year != $array_year || $Page->semester != $array_semester) {
 
-                if ($array_year != 0) {?>
+                  if ($array_year != 0) {?>
+                    </div>
                   </div>
-                </div>
-                <?php }?>
+                  <?php }?>
 
-                <h2 class="row" style="margin-top: 30px; margin-left: 50px;"> 
-                  <?php echo getSemesterNumberByGroup($Page->year, $Page->semester, $Group->year); ?> семестр
-                </h2>
-                <br>
-                <div class="container">
-                  <div class="row g-5 container-fluid">
-                <?php 
-                $array_year = $Page->year;
-                $array_semester = $Page->semester;
-              }?>
+                  <h2 class="row" style="margin-top: 30px; margin-left: 50px;"> 
+                    <?= $semester_number?> семестр
+                  </h2>
+                  <br>
+                  <div class="container">
+                    <div class="row g-5 container-fluid">
+                  <?php 
+                  $array_year = $Page->year;
+                  $array_semester = $Page->semester;
+                }?>
               
-              <div class="col-xs-12 col-sm-12 col-md-6 col-xl-3">
-                <div id="card_subject" class="card" style="border-radius: 0px 0px 10px 10px;" >
-                    <?php 
+                <div class="col-xs-12 col-sm-12 col-md-6 col-xl-3">
+                  <div id="card_subject" class="card" style="border-radius: 0px 0px 10px 10px;" >
+                      <?php 
 
-                    $count_succes_tasks = $Page->getCountSuccessAssignments($User->id);
-                    $count_tasks = $Page->getCountActiveAssignments($User->id);
-
-                    // $count_succes_tasks = 0;
-                    // $count_unsucces_tasks = 0;
-                    // $query_tasks = select_page_tasks($Page->id, 1);
-                    // $result_tasks = pg_query($dbconnect, $query_tasks);
-                    // if (!$result_tasks || pg_num_rows($result_tasks) < 1);
-                    // else {
-                    //     $i = 0;
-                    //     while ($row_task = pg_fetch_assoc($result_tasks)) {
-                    //         $result_assignment = pg_query($dbconnect, select_task_assignment_with_limit($row_task['id'], $_SESSION['hash']));
-                    //         if ($result_assignment && pg_num_rows($result_assignment) >= 1) {
-                    //             $row_task_assignment = pg_fetch_assoc($result_assignment);
-                    //             if ($row_task_assignment['status_code'] == 2 && $row_task_assignment['status'] == 4) $count_succes_tasks++;
-                    //             else if($row_task_assignment['status_code'] == 2) $count_unsucces_tasks++;
-                    //         }
-                    //     }
-                    // }
-                    // $count_tasks = $count_succes_tasks + $count_unsucces_tasks;
-                    // $result = pg_query($dbconnect, select_discipline_name($discipline['disc_id']));
-                    $full_name = $Page->disc_name;
-                    ?>
-                    <div data-mdb-ripple-color="light" style="position: relative;">
-                        <div class="bg-image hover-zoom" style="cursor: pointer;" onclick="window.location='studtasks.php?page=<?=$Page->id?>'">
-                            <img src="<?=$Page->getColorThemeSrcUrl()?>" alt="ИНФОРМАТИКА" style="transition: all .1s linear; height: 200px;">
-                            <div class="mask" style="transition: all .1s linear;"></div>
-                        </div>
-                        <div class="card_image_content"style="cursor: pointer;" onclick="window.location='studtasks.php?page=<?=$Page->id?>'">
-                            <div class="p-2" style="text-align: left;">
-                                <a style="color: white; font-weight: bold;"><?=$Page->name?></a>
-                                <br><a><?php echo $full_name/*[0]['name']*/; ?></a>
+                      $count_succes_tasks = $Page->getCountSuccessAssignments($User->id);
+                      $count_tasks = $Page->getCountActiveAssignments($User->id);
+                      $full_name = $Page->disc_name;
+                      ?>
+                      <div data-mdb-ripple-color="light" style="position: relative;">
+                          <div class="bg-image hover-zoom" style="cursor: pointer;" onclick="window.location='studtasks.php?page=<?=$Page->id?>'">
+                              <img src="<?=$Page->getColorThemeSrcUrl()?>" alt="ИНФОРМАТИКА" style="transition: all .1s linear; height: 200px;">
+                              <div class="mask" style="transition: all .1s linear;"></div>
+                          </div>
+                          <div class="card_image_content"style="cursor: pointer;" onclick="window.location='studtasks.php?page=<?=$Page->id?>'">
+                              <div class="p-2" style="text-align: left;">
+                                  <a style="color: white; font-weight: bold;"><?=$Page->name?></a>
+                                  <br><a><?php echo $full_name/*[0]['name']*/; ?></a>
+                              </div>
+                          </div>
+                      </div>
+                      <div class="card-body">
+                        <?php if ($count_tasks == 0) { ?>
+                            <div class="popover-footer text-muted">
+                                <span>Задания временно отсутствуют</span>
                             </div>
-                        </div>
-                    </div>
-                    <div class="card-body">
-                      <?php if ($count_tasks == 0) { ?>
-                          <div class="popover-footer text-muted">
-                              <span>Задания временно отсутствуют</span>
-                          </div>
-                      <?php } else {
-                        $page_progress = $count_succes_tasks/$count_tasks*100;
-                        if ($page_progress < 30)
-                          $bg_progress_color = "bg-danger";
-                        else if ($page_progress < 100)
-                          $bg_progress_color = "bg-warning";
-                        else 
-                          $bg_progress_color = "bg-success";?>
-                          
-                          <div class="d-flex justify-content-between text-muted" style="width: 100%">
-                              <span>Выполнено</span>
-                              <span><?php echo $count_succes_tasks; ?>/<?php echo $count_tasks; ?></span>
-                          </div>
-                          <div class="progress" style="width: 100%; height: 1px; border-radius: 5px; margin-bottom: 5px;">
-                              <div class="progress-bar <?=$bg_progress_color?>" role="progressbar" style="width: <?=$page_progress?>%" 
-                              aria-valuenow="<?=$count_succes_tasks?>" aria-valuemin="0" aria-valuemax="<?=$count_tasks?>">
-                              </div>
-                          </div>
-                          <div class="progress" style="width: 100%; height: 20px; border-radius: 5px;">
-                              <div class="progress-bar <?=$bg_progress_color?>" role="progressbar" style="width: <?=$page_progress?>%" 
-                              aria-valuenow="<?=$count_succes_tasks?>" aria-valuemin="0" aria-valuemax="<?=$count_tasks?>">
-                                  <?=round($count_succes_tasks/$count_tasks*100, 0)?>%
-                              </div>
-                          </div>
-                      <?php } ?>
-
-                    </div>
+                        <?php } else {
+                          $page_progress = $count_succes_tasks/$count_tasks*100;
+                          if ($page_progress < 30)
+                            $bg_progress_color = "bg-danger";
+                          else if ($page_progress < 100)
+                            $bg_progress_color = "bg-warning";
+                          else 
+                            $bg_progress_color = "bg-success";?>
                             
-                  </div>
+                            <div class="d-flex justify-content-between text-muted" style="width: 100%">
+                                <span>Выполнено</span>
+                                <span><?php echo $count_succes_tasks; ?>/<?php echo $count_tasks; ?></span>
+                            </div>
+                            <div class="progress" style="width: 100%; height: 1px; border-radius: 5px; margin-bottom: 5px;">
+                                <div class="progress-bar <?=$bg_progress_color?>" role="progressbar" style="width: <?=$page_progress?>%" 
+                                aria-valuenow="<?=$count_succes_tasks?>" aria-valuemin="0" aria-valuemax="<?=$count_tasks?>">
+                                </div>
+                            </div>
+                            <div class="progress" style="width: 100%; height: 20px; border-radius: 5px;">
+                                <div class="progress-bar <?=$bg_progress_color?>" role="progressbar" style="width: <?=$page_progress?>%" 
+                                aria-valuenow="<?=$count_succes_tasks?>" aria-valuemin="0" aria-valuemax="<?=$count_tasks?>">
+                                    <?=round($count_succes_tasks/$count_tasks*100, 0)?>%
+                                </div>
+                            </div>
+                        <?php } ?>
+
+                      </div>
+                              
+                    </div>
                 </div>
                     
                 
-            <?php }?>
+            <?php }
+          }?>
 
                   </div>
                 </div>
